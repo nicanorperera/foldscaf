1247db95de7571d0bc178af51f42cdcf7b29e4c0
Nicanor Perera <nicanor.perera@xaver.com.ar>
2012-04-17 14:25:36 -0300

    0.0.8 - Fix y Con Controller

--- lib/generators/fold/fold_generator.rb
+++ lib/generators/fold/fold_generator.rb
@@ -40,7 +40,8 @@ class FoldGenerator < Rails::Generators::Base
   def body
     template 'model.erb'     , "app/models/#{@singular}.rb"
     template 'migration.erb' , "db/migrate/#{fecha}_create_#{@plural}.rb"
-    template 'controller.erb', "app/controllers/admin/#{@plural}_controller.rb"
+    template 'controller.erb', "app/controllers/#{@plural}_controller.rb"
+    template 'admin_controller.erb', "app/controllers/admin/#{@plural}_controller.rb"
     
     %w[_form _mini_form edit index new].each do |action| 
     template "views/#{action}.erb", "app/views/admin/#{@plural}/#{action}.html.erb"
